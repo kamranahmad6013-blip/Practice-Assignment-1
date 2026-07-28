05257c3b7b6ad2b946c72ab3d76b07ea9e26fee7
Kamran Ahmad <kamranahmad6013@gmail.com>
2026-07-28 14:41:53 +0500

    first commit

---
+++ #program 1.py
@@ -0,0 +1,8 @@
+#program 1
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+print("Sum is         =", num1 + num2)
+print("Product is     =", num1 * num2)
+print("Difference is  =", num1 - num2)
+print("Quotient is    =", num1 / num2)
+print("Remainder is   =", num1 % num2)
