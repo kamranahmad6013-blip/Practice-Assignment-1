353917461aa0c9228b014e6be6ed29f3c19353b2
Kamran Ahmad <kamranahmad6013@gmail.com>
2026-07-28 16:12:22 +0500

    Distance conversion program

---
+++ #program 2.py
@@ -0,0 +1,10 @@
+distance = float(input("Enter your distance in kilometers "))
+
+meters = distance*1000
+feet = distance * 3280.84
+inches = distance * 39370.1
+centimeters = distance*100000
+print("Distance in meters   ",meters)
+print("Distance in feet     ",feet)
+print("Distance in inches   ",inches)
+print("Distane in centimeters",centimeters)
