a146b0163aa3e4f8b2d34ee84d820665d33e5df2
kamranahmad6013-blip <kamranahmad6013@gmail.com>
2026-07-28 16:51:57 +0500

    Delete #program 5.py

--- #program 5.py
+++
@@ -1,7 +0,0 @@
-#program 5
-length = float(input("Enter length: "))
-breadth = float(input("Enter breadth: "))
-area = length*breadth
-perimeter = 2*(length+breadth)
-print("Area      =", area)
-print("Perimeter =", perimeter)
