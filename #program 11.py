4b12782efd573950e86b9082ee719797dd3b9398
Kamran Ahmad <kamranahmad6013@gmail.com>
2026-07-31 12:53:30 +0500

    Odd or Even numer checking

---
+++ #program 11.py
@@ -0,0 +1,6 @@
+#program 11
+number = int(input("Enter Any Number "))
+if number%2 == 0 :
+        print("This Number Is Even  : " ,number)
+else:
+            print("This is number is odd :" ,number)
