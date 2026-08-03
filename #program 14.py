386c2831cecba2d12c96b8b8eb8b003d3c40ffb1
Kamran Ahmad <kamranahmad6013@gmail.com>
2026-08-03 12:57:26 +0500

    Find sum , average ,product of three numbers and find which is smallest andlargest also.

---
+++ #program 14.py
@@ -0,0 +1,15 @@
+#program 14
+num1 = int(input("Enter first number          "))
+num2 = int(input("Enter second number         "))
+num3 = int(input("Enter third number           "))
+sum = num1 + num2 + num3 
+average = sum/3
+product = num1*num2*num3
+smallest = min(num1,num2,num3)
+largest = max(num1,num2,num3)
+print("\n\n\n\n\n")
+print("Sum is", sum)
+print("Average is", average)
+print("Product is", product)
+print("Smallest is", smallest)
+print("Largest is", largest)
