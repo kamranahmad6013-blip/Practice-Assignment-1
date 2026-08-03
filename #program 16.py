cf9ad7b3c294b15006528dd05149e9eee63593aa
Kamran Ahmad <kamranahmad6013@gmail.com>
2026-08-03 13:21:49 +0500

    Check whether a triangle is valid

---
+++ #program 16.py
@@ -0,0 +1,13 @@
+#program 16
+
+
+
+
+angle1 = int(input("Enter first angle:           "))
+angle2 = int(input("Enter second angle:          "))
+angle3 = int(input("Enter third angle:              "))
+
+if angle1 + angle2 + angle3 == 180:
+    print("Triangle is Valid")
+else:
+        print("Triangle is Not Valid")        
