f7a3f1045734c032845d94b6567474a6e376e424
Kamran Ahmad <kamranahmad6013@gmail.com>
2026-08-03 13:17:43 +0500

    Body Mass Index (BMI) Calculator

---
+++ #program 15.py
@@ -0,0 +1,14 @@
+#program 15
+weight = float(input("Enter weight in kg:              "))
+height = float(input("Enter height in meters:         "))
+bmi = weight / (height * height)
+print("BMI =", bmi)
+if bmi < 18.5:
+    print("Underweight")
+elif bmi < 25:
+    print("Normal")
+elif bmi < 30:
+    print("Overweight")
+else:
+    print("Obese")
+ 
