fe741779d421239c5a1fe064d062c9b5d53a0e10
Kamran Ahmad <kamranahmad6013@gmail.com>
2026-08-03 16:20:22 +0500

    Finding range from set of numbers

---
+++ #program 25.py
@@ -0,0 +1,18 @@
+#program 25
+
+n = int(input("How many numbers do you want to enter  ?        "))
+
+num = int(input("Enter number:                                 "))
+largest = num 
+smallest = num
+for i in range(1,n):
+    num = int(input("Enter number: "))
+    if num > largest:
+        largest = num
+    if num < smallest:
+        smallest = num
+difference = largest - smallest
+
+print("Largest Number =", largest)
+print("Smallest Number =", smallest)
+print("Range =", difference)
